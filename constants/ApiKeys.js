0bb4935ce8945a3278337b6932514a80b61d0ab2
HongSenDu <hongsendu@gmail.com>
2020-02-08 16:19:33 -0500

    installed firebase

---
+++ constants/ApiKeys.js
@@ -0,0 +1,12 @@
+export default {
+  FirebaseConfig: {
+    apiKey: "AIzaSyDMpvcU9ici6a_uIHYk6HHWOT4y27dLUaU",
+    authDomain: "foodpantry-b9b68.firebaseapp.com",
+    databaseURL: "https://foodpantry-b9b68.firebaseio.com",
+    projectId: "foodpantry-b9b68",
+    storageBucket: "foodpantry-b9b68.appspot.com",
+    messagingSenderId: "569435884894",
+    appId: "1:569435884894:web:3910b3068b302d94c713cf",
+    measurementId: "G-VX3Y3SYC0T"
+  }
+}
