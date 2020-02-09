2b76f768e2d2b6f07498627c9d9c665f158fcf65
yunlanli <yunlan00@.localdomain>
2020-02-08 22:36:04 -0500

    Donator Marker component working. Still need to work on design

---
+++ components/PantryMap/Donator.js
@@ -0,0 +1,65 @@
+/* Marker Component on the map showing location of donator
+ * When pressed, a callout - containing name, pick-up address,
+ * contact info, and available items - will appear.
+ * 
+ * Takes in the following parameters as props
+ * location -> {latitude, longitude} of the donator
+ * donator -> name of the donator
+ * items -> available items
+ * address -> pick up address
+ * phone -> phone # of the donator
+ * addInfo -> Additional info such as allergy
+ */
+import React from 'react';
+import {
+  ScrollView,
+  Text,
+  View,
+} from 'react-native';
+
+import { Marker, Callout, CalloutSubview } from 'react-native-maps';
+import { homeStyle } from '../../GlobalStyles';
+// import Unorderedlist from 'react-native-unordered-list';
+
+function itemGenerator(list) {
+    const itemComponent = list.map(item =>{
+        return(
+            <Text>{item}</Text>
+        );  
+    });
+
+    return itemComponent;
+}
+
+const Donator = props => (
+    <Marker coordinate={props.location}>
+
+        <Callout>
+            <View style={homeStyle.donatorDescription}>
+                <Text style={homeStyle.donatorName}>{props.donator}</Text>
+                
+                <CalloutSubview>
+                    <ScrollView>
+                        <Text>Available items:</Text>
+                        <ScrollView style={homeStyle.donatorItems}>
+                            {itemGenerator(props.items)}
+                        </ScrollView>
+                    </ScrollView>
+                </CalloutSubview>
+                
+                <View>
+                    <Text>Pick-Up:</Text>
+                    <Text>{props.address}</Text>
+
+                    <Text>Phone: {props.phone}</Text>
+                    <Text>Additional Info: {props.addInfo}</Text>
+                </View>
+
+            </View>
+        </Callout>
+
+    </Marker>
+    
+)
+
+export default Donator;
