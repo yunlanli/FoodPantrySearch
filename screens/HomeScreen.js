159541dde310b01faf2e6c19cb38b44f6f0262a0
Aditi Dam <dama2468@gmail.com>
2020-02-09 00:46:56 -0500

    Update HomeScreen.js

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -15,7 +15,32 @@ export default class HomeScreen extends Component {
       <View style={styles.container}>
 
       
+ 
+  <Image source={require('assets/images/MainTitle.png')} 
+      style={{
+      width: 350,
+      height: 350,
+      position: 'absolute',
+      left: 0,
+      bottom: 270,
 
+  }}
+
+/>
+
+
+  <Image source={require('assets/images/LOGO.png')} 
+      style={{
+      width: 500,
+      height: 500,
+      position: 'absolute',
+      right: -60,
+      top: 20,
+
+  }}
+  
+/>
+  
         <View style={styles.buttonContainer1}>
           <Button
             onPress={this._onPressButton}
@@ -24,6 +49,8 @@ export default class HomeScreen extends Component {
 
           />
         </View>
+
+
         <View style={styles.buttonContainer2}>
           <Button
             onPress={this._onPressButton}
@@ -31,7 +58,10 @@ export default class HomeScreen extends Component {
             color="#000000"
            
           />
+
+
         </View>
+
         <View style={styles.buttonContainer3}>
          
           <Button
@@ -40,34 +70,10 @@ export default class HomeScreen extends Component {
             color="#000000"
           
           />
+</View>
 
-
-
-  <Image source={require('assets/images/MainTitle.png')} 
-      style={{
-      width: 350,
-      height: 350,
-      position: 'absolute',
-      left: -8,
-      bottom: 240,
-
-  }}
-
-/>
-
-  <Image source={require('assets/images/LOGO.png')} 
-      style={{
-      width: 450,
-      height: 600,
-      position: 'absolute',
-      left: -40,
-      bottom: -25,
-
-  }}
-  
-/>
-        </View>
-      </View>
+</View>
+ 
     );
     
   }
@@ -86,7 +92,7 @@ const styles = StyleSheet.create({
     margin: 20,
     backgroundColor: '#66ccff',
    right: 3,
-    top: 150,
+    top: 300,
     borderRadius:15,
   
      
@@ -103,10 +109,9 @@ const styles = StyleSheet.create({
    buttonContainer3: {
     margin: 20,
     backgroundColor: '#cc66ff',
-    right: 3,
-    top: 150,
+    // right: 3,
+    // top: 75,
     borderRadius:15,
   
   },
 });
-
